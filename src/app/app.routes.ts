81c5881d50ba74e409b7ba41a9b77a1d71c4f610
Gianni Marchiaro Automobili <giannimarchiaroautomobili@MacBook-Air-di-Gianni.fritz.box>
2026-02-27 14:30:32 +0100

    sistemazione sidebar e toolbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,18 @@ import { Routes } from '@angular/router';
 import { Dashboard } from './componenti/dashboard/dashboard';
 import { Login } from './componenti/login/login';
 import { Registrazione } from './componenti/registrazione/registrazione';
+import { Pagina1 } from './componenti/pagina1/pagina1';
+import { Pagina2 } from './componenti/pagina2/pagina2';
+import { Pagina3 } from './componenti/pagina3/pagina3';
+import { authGuard } from './auth/auth-guard';
 
 export const routes: Routes = [
-    {path:'', component: Dashboard},
+    {path: '', component: Dashboard, canActivate: [authGuard],children: [
+        {path: '', redirectTo: 'pagina1', pathMatch: 'full'},
+        {path: 'pagina1', component: Pagina1},
+        {path: 'pagina2', component: Pagina2},
+        {path: 'pagina3', component: Pagina3},
+    ]},
     {path:'login', component: Login},
     {path:'registrazione', component: Registrazione},
 ];
--- src/app/componenti/dashboard/dashboard.ts
+++ src/app/componenti/dashboard/dashboard.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
+import { RouterLink, RouterOutlet, RouterLinkActive } from "@angular/router";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard',
   standalone:true,
-  imports: [MatSidenavModule, MatListModule],
+  imports: [MatSidenavModule, MatListModule, RouterLink, RouterOutlet, RouterLinkActive,MatToolbarModule,MatIconModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
